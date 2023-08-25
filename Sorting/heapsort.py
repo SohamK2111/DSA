d15b6970df9c546985a63eeac5456c87ef9103ca
Soham <sk2111@cam.ac.uk>
2023-08-25 13:12:04 +0100

    heapq module imported

--- Sorting/heapsort.py
+++ Sorting/heapsort.py
@@ -45,10 +45,13 @@ print(heapsort(A))
 #Priority Queues
 
 # A priority queue is a data structure for maintaining a set S of elements, each with an associated value called a key.
+# A max-priority queue supports the following operations:
 
+# heap_maximum(S) returns the element of S with the largest key
 def heap_maximum(A):
     return A[0]
 
+# heap_extract_max(S) removes and returns the element of S with the largest key and removes it from the queue
 def heap_extract_max(A):
     global heapsize
     if heapsize < 1:
@@ -59,6 +62,7 @@ def heap_extract_max(A):
     max_heapify(A, 1)
     return max
 
+# heap_increase_key(S, x, k) increases the value of element x's key to the new value k, which is assumed to be at least as large as x's current key value
 def heap_increase_key(A, i, key):
     if key < A[i-1]:
         print("new key is smaller than current key")
@@ -67,6 +71,7 @@ def heap_increase_key(A, i, key):
         A[i-1], A[parent(i)-1] = A[parent(i)-1], A[i-1]
         i = parent(i)
 
+# max_heap_insert(S, x) inserts the element x into the set S, which is equivalent to the operation S = S U {x}
 def max_heap_insert(A, key):
     global heapsize
     heapsize += 1
@@ -78,3 +83,25 @@ def max_heap_insert(A, key):
 print(max_heap_insert(A, 100))
 print(heap_extract_max(A))
 print(A)
+
+#------------------------------------------------------------
+
+import heapq
+
+# heapq is a module that provides an implementation of a min-priority queue using a binary heap
+# to turn it into a max-priority queue, you can negate the keys
+
+li = [1,2,3,4,5,6,7,8,9,10]
+
+heapq.heapify(li)
+minheap = li
+print("minheap:", minheap)
+heapq._heapify_max(li)
+maxheap = li
+print("maxheap:", maxheap)
+heapq.heapify([-i for i in li])
+maxheap1 = li
+print("maxheap:", maxheap1)
+
+
+# there are 3 main functions in heapq: heappush, heappop and heapify. For more info, see https://docs.python.org/3/library/heapq.html
