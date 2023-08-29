ff8a88dec3dfb72cdbc6e9856bc9943f64581feb
Soham <sk2111@cam.ac.uk>
2023-08-29 20:46:16 +0100

    Machine Learning Decision Tree coded from scratch

--- practise.py
+++ practise.py
@@ -1,22 +1,4 @@
 
-def wordPattern(pattern, s):
-    """
-    :type pattern: str
-    :type s: str
-    :rtype: bool
-    """
-    dictionary = {}
-    t = s.split(" ")
-    for i in range(len(pattern)):
-        if pattern[i] not in dictionary and t[i] not in dictionary.values():
-            dictionary[pattern[i]] = t[i] 
-        elif pattern[i] not in dictionary and t[i] in dictionary.values():
-            return False
-        elif pattern[i] in dictionary and dictionary[pattern[i]] != t[i]:
-            return False
-    return True
-
-
-print(wordPattern('abba', 'dog dog dog dog'))
-    
+a = [1,2,3,4,5,6,7,8,9,10]
+print(list(-a))
             
